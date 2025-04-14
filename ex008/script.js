f8c6c2097cf4b440b3c43ddcb7ce59adf481400e
João Marcos Siqueira Cunha <joaomarcossiqueiracunha0@gmail.com>
2025-04-14 20:39:13 -0300

    p

--- ex008/script.js
+++ ex008/script.js
@@ -7,3 +7,20 @@ document.addEventListener('DOMContentLoaded', () => {
         nav.classList.toggle('nav-active');
     });
 });
+
+function adjustTextSize() {
+    const textElements = document.querySelectorAll('.responsive-text');
+    textElements.forEach(element => {
+        const screenWidth = window.innerWidth;
+        if ( screenWidth >= 1024) {
+            element.style.fontSize = '1.5rem';
+        } else if (screenWidth >= 600) {
+            element.style.fontSize = '1.25rem';
+        } else {
+            element.style.fontSize = '1rem';
+        }
+    });
+}
+
+window.addEventListener('resize', adjustTextSize);
+window.addEventListener('DOMContentLoaded', adjustTextSize);
