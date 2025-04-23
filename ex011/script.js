c6a3a41f5e574521171baa110d0db9af519b26e6
João Marcos Siqueira Cunha <joaomarcossiqueiracunha0@gmail.com>
2025-04-23 20:45:47 -0300

    toque e gestos

---
+++ ex011/script.js
@@ -0,0 +1,23 @@
+let gallery = document.querySelector('.gallery');
+let startX;
+let currentTranslate = 0;
+let prevTranslate = 0;
+let dragging = false;
+
+
+gallery.addEventListener('touchstart', (e) => {
+    startX = e.touches[0].clientX ;
+    dragging = true ;
+});
+
+gallery.addEventListener('touchmove', (e) => {
+    if (!dragging) return;
+    let touchX = e.touches[0].clientX;
+    currentTranslate = prevTranslate + touchX - startX;
+    gallery.style.transform = `translateX(${currentTranslate}px)`;
+});
+
+gallery.addEventListener('touchend', () => {
+    prevTranslate = currentTranslate;
+    dragging = false;
+});
